08b86a8679aa0674a1e5b4127875b1873003ccdb
Jean <jean0798@gmail.com>
2020-08-03 19:15:59 -0600

    coreccion del url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://jeanvegad.github.io.',
+  url: 'https://jeanvegad.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
