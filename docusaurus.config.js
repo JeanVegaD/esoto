89eca0f3b800a7bb447f0bcfdc0d1c36c197144c
Jean Vega <jean0798@gmail.com>
2020-08-15 11:29:56 -0600

    update docusaurus.config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://jeanvegad.github.io',
+  url: 'https://jeanvegad.github.io/esoto',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
